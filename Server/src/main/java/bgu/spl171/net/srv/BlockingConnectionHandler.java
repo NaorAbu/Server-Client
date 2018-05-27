c219690a36a2f4813a73a5225c3a5d2eaf859253
NaorAbu <35075782+NaorAbu@users.noreply.github.com>
2018-05-27 15:00:51 +0300

    Add files via upload

---
+++ Server/src/main/java/bgu/spl171/net/srv/BlockingConnectionHandler.java
@@ -0,0 +1,69 @@
+package bgu.spl171.net.srv;
+
+import bgu.spl171.net.api.MessageEncoderDecoder;
+import bgu.spl171.net.api.bidi.BidiMessagingProtocol;
+import bgu.spl171.net.api.bidi.ConnectionsImpl;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class BlockingConnectionHandler<T> implements Runnable, ConnectionHandler<T> {
+
+    private final BidiMessagingProtocol<T> protocol;
+    private final MessageEncoderDecoder<T> encdec;
+    private final Socket sock;
+    private BufferedInputStream in;
+    private BufferedOutputStream out;
+    private volatile boolean connected = true;
+    private ConnectionsImpl<T> connections;
+
+    public BlockingConnectionHandler(Socket sock, MessageEncoderDecoder<T> reader, BidiMessagingProtocol<T> protocol) {
+        this.sock = sock;
+        this.encdec = reader;
+        this.protocol = protocol;
+    }
+
+    @Override
+    public void run() {
+        protocol.start(connections.getID(this), connections);
+        try (Socket sock = this.sock) { //just for automatic closing
+            int read;
+
+            in = new BufferedInputStream(sock.getInputStream());
+            out = new BufferedOutputStream(sock.getOutputStream());
+
+            while (connected && (read = in.read()) >= 0) {
+                T nextMessage = encdec.decodeNextByte((byte) read);
+                if (nextMessage != null) {
+                    protocol.process(nextMessage);
+                }
+            }
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void close() throws IOException {
+        connected = false;
+        sock.close();
+    }
+
+    @Override
+    public void send(T msg) {
+        try {
+            out.write(encdec.encode(msg));
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setConnections(ConnectionsImpl<T> connections) {
+        this.connections = connections;
+    }
+}
